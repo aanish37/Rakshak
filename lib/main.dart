56e664b61bb118607b8f9793c8357f0768c4217c
aanish37 <adhikariaanish37@gmail.com>
2023-10-26 16:10:53 +0545

    app initialized

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:rakhshak/constant.dart';
+import './view/home_page.dart';
+import './view/contact_screen.dart';
+import './view/dashboard.dart';
+import './view/phone_mobile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application
+  // .
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+          scrollbarTheme: ScrollbarThemeData(
+            trackVisibility: MaterialStateProperty.all(true),
+            thumbVisibility: MaterialStateProperty.all(true),
+          ),
+        colorScheme: ColorScheme.fromSeed(seedColor: backgroundColor),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/home': (context) => HomePage(),
+        '/contacts': (context) => const ContactScreen(),
+        '/dashboard': (context) => const Dashboard(),
+        '/phone': (context) => PhoneMobile(),
+      },
+      home: HomePage(),
+    );
+  }
+}
