1caca4be47341f2f848d9d2b5b78ff2e2af61d38
aanish37 <aanish37@gmail.com>
2023-11-06 00:38:21 +0545

    added logic for saving one contact for sending message

--- lib/widget/Emergency.dart
+++ lib/widget/Emergency.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rakhshak/constant.dart';
 
-import 'package:url_launcher/link.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Emergency extends StatelessWidget {
